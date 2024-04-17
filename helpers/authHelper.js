ac52c223f7a9ba1c794628e636b606e0561723be
sahidsiddik8786 <120910602+sahidsiddik8786@users.noreply.github.com>
2024-04-17 05:46:04 +0530

    Update authHelper.js

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,15 +1,15 @@
-import bcrypt from 'bcrypt'
+import crypto from 'crypto';
 
 export const hashPassword = async(password) => {
     try {
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password,saltRounds);
-        return hashedPassword
+        const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
+        return hashedPassword;
     } catch (error) {
         console.log(error);
     }
 };
 
-export const comparePassword = async (password,hashedPassword) => {
-    return bcrypt.compare(password,hashedPassword);
+export const comparePassword = async (password, hashedPassword) => {
+    const inputHash = crypto.createHash('sha256').update(password).digest('hex');
+    return inputHash === hashedPassword;
 }
